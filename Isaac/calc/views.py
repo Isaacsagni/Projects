534fb42a578e77f9e46076d342977c38c4cb9ab7
Isaacsagni <brianisaac142@gmail.com>
2024-07-08 03:47:27 +0300

    DTL part 2,

--- Isaac/calc/views.py
+++ Isaac/calc/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {'name':'Isaac'}) # home page rendered
+    return render(request, 'home.html', {'name':'Brian'}) # home page rendered
