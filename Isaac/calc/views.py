97328b9518382aef5e17ec2585dacd9070503e14
Isaacsagni <brianisaac142@gmail.com>
2024-07-08 05:10:08 +0300

    Adding 2 numbers, settings included

--- Isaac/calc/views.py
+++ Isaac/calc/views.py
@@ -6,3 +6,11 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html', {'name':'Brian'}) # home page rendered
+
+
+def add(request):
+    #python code to add 2 numbers
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+    res = val1 + val2
+    return render(request, 'result.html', {'result':res}) # When add is called, result will appear after converted to Jinja format
