a4da813e84b46c1ede2962fa449be44b6d32a80d
Isaacsagni <brianisaac142@gmail.com>
2024-07-17 05:49:25 +0300

    Passing Dynamic Data in Html - part 2

--- Isaac/trav/views.py
+++ Isaac/trav/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
+from .models import Cloth
 
-from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html', {'price' : 65}) # home page rendered
+
+    Cloth1 = Cloth() 
+    Cloth1.name = 'Casual Shirt'
+    Cloth1.price = 65
+    Cloth1.img = 'p1.png'
+
+    Cloth2 = Cloth()
+    Cloth2.name = 'Track Suit'
+    Cloth2.price = 85
+    Cloth2.img = 'p8.png'
+
+    Cloth3 = Cloth()
+    Cloth3.name = "Men's Shirt"
+    Cloth3.price = 70
+    Cloth3.img = 'p10.png'
+
+    Cloths = [Cloth1, Cloth2, Cloth3]
+    return render(request, 'index.html', {'Cloths' : Cloths}) # home page rendered
